a74ce4c6f877953f6f2e454dd4c9dd3d3588fc02
kellingraywilliamson <55054332+kellingraywilliamson@users.noreply.github.com>
2022-02-01 16:55:57 +0000

    Added enemy hit points and scoreboard

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,13 +6,48 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private GameObject deathVFX;
+    [SerializeField] private GameObject hitVFX;
     [SerializeField] private Transform parent;
+    [SerializeField] private int scoreValue = 15;
+    [SerializeField] private int hitPoints = 2;
+
+    private ScoreBoard _scoreBoard;
+
+    private void Start()
+    {
+        _scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
 
     private void OnParticleCollision(GameObject other)
     {
-        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
-        vfx.transform.parent = parent;
+        ApplyScoreIncrease();
+
+        if (--hitPoints <= 0)
+        {
+            RunVFX(deathVFX);
+            DestroySelf();
+        }
+        else
+        {
+            RunVFX(hitVFX);
+        }
+    }
+
+    private void ApplyScoreIncrease()
+    {
+        if (!_scoreBoard) return;
+        _scoreBoard.IncreaseScore(amountToIncrease: scoreValue);
+    }
+
+    private void DestroySelf()
+    {
         gameObject.SetActive(false);
         Destroy(gameObject);
+    }
+
+    private void RunVFX(GameObject vfxToRun)
+    {
+        GameObject vfx = Instantiate(vfxToRun, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
     }
 }
--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,18 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreBoard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int _score;
+    private TMP_Text _scoreText;
+
+    private void Start()
     {
-        
+        _scoreText = GetComponent<TMP_Text>();
+        _scoreText.text = "0";
     }
 
-    // Update is called once per frame
-    void Update()
+    public void IncreaseScore(int amountToIncrease)
     {
-        
+        _score += amountToIncrease;
+        _scoreText.text = _score.ToString();
     }
 }
